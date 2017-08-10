13ac5a9ce6f793db3d0da98a8203c476f37aed60
Wiktor <wiktorkalinowski@gmail.com>
2017-08-10 18:07:23 +0200

    Sorted + page

--- src/main/java/com/wiktor/demo/RunAsStart.java
+++ src/main/java/com/wiktor/demo/RunAsStart.java
@@ -1,6 +1,8 @@
 package com.wiktor.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
@@ -23,25 +25,50 @@ public class RunAsStart {
 
     @PostConstruct
     public void runAtStart() {
+        generateManyEmployees();
+
+        logger.info("UNSORTED");
+        printAll(findAllUnsorted());
+
+        logger.info("SORTED BY FIRST NAME");
+        printAll(getSortedByFirstName());
+
+        logger.info("SORTED BY FIRST AND LAST NAME");
+        printAll(getSortedByFirstAndLastName());
+
+        Page<Employee> page = employeeRepository.findAll((new PageRequest(2, 10)));
+        logger.info("TOTAL NUMBER OF EMPLOYEES: " + page.getTotalElements());
+        logger.info("TOTAL NUMBER OF PAGE: " + page.getTotalPages());
+        logger.info("CURRENT PAGE NUMBER: " + page.getNumber());
+        logger.info("ELEMENT OF PAGE");
+        printAll(page.getContent());
+
+    }
+
+    private void generateManyEmployees() {
         for (int i = 0; i < 100; i++) {
             employeeRepository.save(
                     employeeGenerator.generate());
         }
+    }
 
-        List<Employee> allUnsorted = employeeRepository.findAll();
-        printAll(allUnsorted);
+    private List<Employee> getSortedByFirstAndLastName() {
+        return employeeRepository.findAll(
+                new Sort(new Sort.Order(
+                        Sort.Direction.ASC, "secondName"
+                )));
+    }
 
-        List<Employee> sortedByFirstName = employeeRepository.findAll(
+    private List<Employee> getSortedByFirstName() {
+        return employeeRepository.findAll(
                 new Sort(
                         new Sort.Order(
                                 Sort.Direction.ASC, "firstName"
-                        ),
-                        new Sort.Order(
-                                Sort.Direction.ASC, "secondName"
-                        )
-                ));
-        logger.info("SORTED BY FIRST NAME");
-        printAll(sortedByFirstName);
+                        )));
+    }
+
+    private List<Employee> findAllUnsorted() {
+        return employeeRepository.findAll();
     }
 
     private void printAll(List<Employee> allUnsorted) {
